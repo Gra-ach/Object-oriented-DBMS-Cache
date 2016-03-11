5c683c490b6db262d8c2ad1f3e81b6628520515f
Gra-ach <Gra-ach@users.noreply.github.com>
2016-03-11 21:09:56 +0200

    C# eXTreme Project
    Uploading Sample for C# eXTreme

---
+++ C# eXTreme/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace eXTremeTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(467, 50);
+            this.panel1.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(289, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Show";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(197, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Create";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(105, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "List globals";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(380, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(224, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(230, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Перелік глобалів у просторі імен USER";
+            // 
+            // treeView1
+            // 
+            this.treeView1.FullRowSelect = true;
+            this.treeView1.Location = new System.Drawing.Point(108, 85);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(346, 203);
+            this.treeView1.TabIndex = 6;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Дерево індексів";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Опис серіалу";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Головні актори";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(232, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Постер сезону";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(363, 366);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Рейтинг серії";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(350, 426);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Час показу серії";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(108, 300);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(346, 58);
+            this.textBox1.TabIndex = 13;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 387);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(203, 108);
+            this.listBox1.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(235, 387);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(355, 387);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(351, 447);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(104, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 506);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Приклад роботи eXTreme";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+
+    }
+}
+
---
+++ C# eXTreme/Form1.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using InterSystems.Globals;
+using InterSystems.Data.CacheClient;
+using System.IO;
+
+
+namespace eXTremeTest
+{
+    public partial class Form1 : Form
+    {
+        private Connection myConn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myConn = ConnectionContext.GetConnection();
+            if (!myConn.IsConnected())
+            {
+                myConn.Connect("User", "_SYSTEM", "SYS");
+            }
+
+            if (myConn.IsConnected())
+            {
+                MessageBox.Show("З’єднання з БД виконано");
+            }
+            else
+            {
+                MessageBox.Show("З’єднання з БД не було виконано");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (myConn != null && myConn.IsConnected())
+                myConn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GlobalsDirectory globDir = null;
+            try
+            {
+                globDir = myConn.CreateGlobalsDirectory();
+                comboBox1.Items.Clear();
+                string globName = "Series";
+                while (!globName.Equals(""))
+                {
+                    comboBox1.Items.Add(globName);
+                    globName = globDir.NextGlobalName();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (globDir != null)
+                   globDir.Close();
+            }
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            NodeReference nodeRef = null;
+            try
+            {
+                ComboBox comboBox = (ComboBox)sender;
+                string global = comboBox.SelectedItem.ToString();
+                nodeRef = myConn.CreateNodeReference(global);
+                if (nodeRef.HasData() || nodeRef.HasSubnodes())
+                {
+                    DialogResult dr = MessageBox.Show("Ви бажаєте видалити існуючий глобал?", "Globals Test", MessageBoxButtons.YesNo);
+                    if (dr == DialogResult.Yes)
+                    {
+                        nodeRef.Kill();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (nodeRef != null)
+                    nodeRef.Close();
+            }
+        }
+
+        private byte[] ReadImage(string p_postedImageFileName)
+        {
+            FileStream fs = null;
+            BinaryReader br = null;
+            try
+            {
+                fs = new FileStream(p_postedImageFileName, FileMode.Open, FileAccess.Read);
+                br = new BinaryReader(fs);
+                byte[] image = br.ReadBytes((int)fs.Length);
+                return image;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            finally
+            {
+                br.Close();
+                fs.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            NodeReference nodeRef = null;
+            try
+            {
+                nodeRef = myConn.CreateNodeReference(comboBox1.SelectedItem.ToString());                
+
+                //побудова віртуального дерева індексів
+                nodeRef.AppendSubscript("Arrow");
+                nodeRef.Set("Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.");
+
+                nodeRef.AppendSubscript(2012);
+                ValueList myList = myConn.CreateList();
+                myList.Append("Stephen Amell", "Katie Cassidy", "David Ramsey", "Willa Holland", "Paul Blackthorne", "Susanna Thompson", "Emily Bett Rickards");
+                nodeRef.Set(myList);
+                myList.Close();
+
+                nodeRef.AppendSubscript(1);
+                nodeRef.Set(File.ReadAllBytes("E:\\КПИ\\Dropbox\\Асистент\\Постреляционные БД\\Учебное пособие\\Direct access\\arrow_poster_season1.jpg"));
+
+                nodeRef.AppendSubscript(1);
+                nodeRef.Set(8.4);
+
+                nodeRef.AppendSubscript("10/10/2012");
+                nodeRef.Set(8);
+
+                nodeRef.SetSubscriptCount(3);
+                nodeRef.AppendSubscript(2);
+                nodeRef.Set(8.3);
+
+                nodeRef.AppendSubscript("10/17/2012");
+                nodeRef.Set(8);
+
+                nodeRef.SetSubscriptCount(3);
+                nodeRef.AppendSubscript(3);
+                nodeRef.Set(8.4);
+
+                nodeRef.AppendSubscript("10/24/2012");
+                nodeRef.Set(8);
+
+                nodeRef.SetSubscriptCount(2);
+                nodeRef.AppendSubscript(2);
+                nodeRef.Set(File.ReadAllBytes("E:\\КПИ\\Dropbox\\Асистент\\Постреляционные БД\\Учебное пособие\\Direct access\\arrow-season-2-poster-cw.jpg"));
+
+                nodeRef.AppendSubscript(1);
+                nodeRef.Set(8.9);
+
+                nodeRef.AppendSubscript("10/09/2013");
+                nodeRef.Set(8);
+
+                nodeRef.SetSubscriptCount(3);
+                nodeRef.AppendSubscript(2);
+                nodeRef.Set(8.7);
+
+                nodeRef.AppendSubscript("10/16/2013");
+                nodeRef.Set(8);
+
+                //безпосереднє додавання значень без зміни положення поточного елементу                
+                nodeRef.SetSubscriptCount(0);
+                nodeRef.Set("A lone crimefighter battles the forces of evil with the help of an indestructible and artificially intelligent supercar.", "Knight rider");
+
+                myList = myConn.CreateList();
+                myList.Append("Justin Bruening", "Deanna Russo", "Bruce Davison", "Sydney Tamiia Poitier", "Yancey Arias");
+                nodeRef.Set(myList, "Knight rider", 2008);
+                myList.Close();
+
+                nodeRef.Set(File.ReadAllBytes("E:\\КПИ\\Dropbox\\Асистент\\Постреляционные БД\\Учебное пособие\\Direct access\\knight_rider_2008.jpg"), "Knight rider", 2008, 1);
+
+                nodeRef.Set(6.0, "Knight rider", 2008, 1, 0);
+                nodeRef.Set(9, "Knight rider", 2008, 1, 0, "02/17/2008");
+
+                nodeRef.Set(6.0, "Knight rider", 2008, 1, 1);
+                nodeRef.Set(7, "Knight rider", 2008, 1, 1, "09/24/2008");
+
+                nodeRef.Set(6.0, "Knight rider", 2008, 1, 2);
+                nodeRef.Set(7, "Knight rider", 2008, 1, 2, "10/01/2008");
+
+                //безпосереднє завдання рівня ієрархії дерева, на якому будуть створюватися нові вузли
+                nodeRef.AppendSubscript("Knight rider");
+                nodeRef.SetSubscript(2, 1982);
+                myList = myConn.CreateList();
+                myList.Append("David Hasselhoff", "Edward Mulhare", "Richard Basehart", "Patricia McPherson");
+                nodeRef.Set(myList);
+                myList.Close();
+
+                nodeRef.SetSubscript(3, 1);
+                nodeRef.Set(File.ReadAllBytes("E:\\КПИ\\Dropbox\\Асистент\\Постреляционные БД\\Учебное пособие\\Direct access\\knight_rider_1982_season_1.jpg"));
+
+                nodeRef.SetSubscript(4, 1);
+                nodeRef.Set(7.2);
+
+                nodeRef.SetSubscript(5, "09/26/1982");
+                nodeRef.Set(7);
+
+                nodeRef.SetSubscriptCount(3);
+                nodeRef.SetSubscript(4, 2);
+                nodeRef.Set(6.8);
+
+                nodeRef.SetSubscript(5, "09/26/1982");
+                nodeRef.Set(7);
+
+                nodeRef.SetSubscriptCount(3);
+                nodeRef.SetSubscript(4, 3);
+                nodeRef.Set(6.8);
+
+                nodeRef.SetSubscript(5, "10/01/1982");
+                nodeRef.Set(7);
+
+                nodeRef.SetSubscriptCount(2);
+                nodeRef.SetSubscript(3, 2);
+                nodeRef.Set(File.ReadAllBytes("E:\\КПИ\\Dropbox\\Асистент\\Постреляционные БД\\Учебное пособие\\Direct access\\knight_rider_1982_season_2.jpg"));
+
+                nodeRef.SetSubscript(4, 1);
+                nodeRef.Set(7.8);
+
+                nodeRef.SetSubscript(5, "10/02/1983");
+                nodeRef.Set(7);
+
+                nodeRef.SetSubscriptCount(3);
+                nodeRef.SetSubscript(4, 2);
+                nodeRef.Set(6.8);
+
+                nodeRef.SetSubscript(5, "10/02/1983");
+                nodeRef.Set(7);
+
+                nodeRef.SetSubscriptCount(3);
+                nodeRef.SetSubscript(4, 3);
+                nodeRef.Set(6.9);
+
+                nodeRef.SetSubscript(5, "10/09/1983");
+                nodeRef.Set(7);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (nodeRef != null)
+                   nodeRef.Close();
+            }
+        }
+
+        private void AddNodes(NodeReference aNodeRef, string aBaseName, TreeNode aTreeNode)
+        {
+            try
+            {
+                aNodeRef.AppendSubscript("");
+                string subscr = aNodeRef.NextSubscript();
+                TreeNode treeNode = null;
+                while (!subscr.Equals(""))
+                {
+                    aNodeRef.SetSubscript(aNodeRef.GetSubscriptCount(), subscr);
+                    if (aNodeRef.HasData())
+                    {
+                        treeNode = new TreeNode(subscr);
+                        if (aTreeNode == null)
+                        {
+                            treeView1.Nodes.Add(treeNode);
+                        }
+                        else
+                        {
+                            aTreeNode.Nodes.Add(treeNode);
+                        }
+                    }
+                    if (aNodeRef.HasSubnodes())
+                    {
+                        AddNodes(aNodeRef, subscr, treeNode);
+                    }
+                    subscr = aNodeRef.NextSubscript();
+                }
+            }
+            catch (GlobalsException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                aNodeRef.SetSubscriptCount(aNodeRef.GetSubscriptCount() - 1);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            NodeReference nodeRef = null;
+            
+            try
+            {
+                nodeRef = myConn.CreateNodeReference(comboBox1.SelectedItem.ToString());                
+                AddNodes(nodeRef, "", null);
+                treeView1.ExpandAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (nodeRef != null)
+                    nodeRef.Close();
+            }
+        }
+
+        private void formNode(TreeNode aCurNode, NodeReference aNodeRef)
+        {
+            if (aCurNode.Parent != null)
+                formNode(aCurNode.Parent, aNodeRef);
+            aNodeRef.AppendSubscript(aCurNode.Text);
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            NodeReference nodeRef = null;
+
+            try
+            {
+                nodeRef = myConn.CreateNodeReference(comboBox1.SelectedItem.ToString());
+                formNode(e.Node, nodeRef);
+
+                Object value = nodeRef.GetObject();
+                if (value is string)
+                {
+                    if (nodeRef.GetSubscriptCount() == 1)
+                    {
+                        textBox1.Text = value.ToString();
+                    }
+                    else
+                        if (nodeRef.GetSubscriptCount() == 2)
+                        {
+                            ValueList outList = nodeRef.GetList();
+                            outList.ResetToFirst();
+                            listBox1.Items.Clear();
+                            for (int i = 0; i < outList.Length; i++)
+                            {
+                                listBox1.Items.Add(outList.GetNextObject());
+                            }
+                            outList.Close();
+                        }
+                        else
+                            if (nodeRef.GetSubscriptCount() == 3)
+                            {
+                                using (MemoryStream stream = new MemoryStream(nodeRef.GetBytes()))
+                                {
+                                    pictureBox1.Image = System.Drawing.Image.FromStream(stream);
+                                }
+                            }
+                }
+                else if (value is double)
+                {
+                    textBox2.Text = value.ToString();
+                }
+                else if (value is int)
+                {
+                    textBox3.Text = value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (nodeRef != null)
+                    nodeRef.Close();
+            }
+        }
+
+    }
+}
